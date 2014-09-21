170014cdaeea76f90259e55a9f425b0f14fc9d0a
Jason Grimes <jason@grimesit.com>
2014-09-20 22:54:04 -0400

    Update for current version of ZF2 tutorial.

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Album;
+
 return array(
      'controllers' => array(
          'invokables' => array(
@@ -30,4 +32,19 @@ return array(
              'album' => __DIR__ . '/../view',
          ),
      ),
+    // Doctrine config
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        )
+    )
  );
